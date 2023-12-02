2f04b4093b172cf7db48ab20ce9c1cf3fd47c82f
vcky4 <okon.victor.u@gmail.com>
2023-12-02 02:53:14 +0100

    fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,5 @@
 export default class Building {
     constructor(sqft) {
-        if (new.target === Building) {
-            throw new Error('Abstract class Building cannot be instantiated directly');
-        }
         this._sqft = sqft;
     }
 
